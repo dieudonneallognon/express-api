2453b8aa83b425c27bbace0202aae029026fe8eb
jiedevm <dieudonne.allognon@yahoo.com>
2022-05-23 14:23:16 +0200

    Add GET /api/tasks endpoint

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const express = require("express");
+const Task = require("./models/Task");
 
 const app = express();
+
+app.get("/api/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.json({ status: 200, data: tasks });
+    } catch (err) {
+        console.log(err);
+        res.json({ status: 505, error: "Internal Server error" });
+    }
+});
 
 app.listen(process.env.SERVER_PORT, process.env.SERVER_HOSTNAME, (req, res) => {
     console.log(
