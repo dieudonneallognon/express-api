7f5aa700e14189fdee0d48d34a86ef772f91289b
jiedevm <dieudonne.allognon@yahoo.com>
2022-05-23 13:49:04 +0200

    Set-up of working MongoDB database with server

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+
 const express = require("express");
 
 const app = express();
@@ -8,4 +10,13 @@ app.listen(process.env.SERVER_PORT, process.env.SERVER_HOSTNAME, (req, res) => {
     console.log(
         `Serving on port ${process.env.SERVER_PORT} at host ${process.env.SERVER_HOSTNAME}`
     );
+
+    mongoose
+        .connect(
+            `mongodb://${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_NAME}`
+        )
+        .then(() => {
+            console.log("Server connected to database...");
+        })
+        .catch((err) => console.log(err));
 });
